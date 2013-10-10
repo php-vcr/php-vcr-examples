040f8e3a333570443db401f1ea5632f6320d7be9
Adrian Philipp <mail@adrian-philipp.com>
2013-10-10 15:22:08 +0200

    Adds soap client example WIP.

---
+++ soap/test/Adri/WeatherApiTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Adri;
+
+/**
+* Checks cdyne.com for local weather information.
+*
+* @link http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL
+*/
+class WeatherApiTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetTemperatureByInvalidZip()
+    {
+        $weatherApi = new WeatherApi();
+        $weatherApi->getTemperature(null);
+    }
+
+    /**
+     * @vcr soap_weather_api_temperature
+     */
+    public function testGetTemperatureByZip()
+    {
+        $weatherApi = new WeatherApi();
+        $actual = $weatherApi->getTemperature('10013'); // somewhere in New York
+
+        $this->assertInternalType('integer', $actual);
+    }
+
+}
