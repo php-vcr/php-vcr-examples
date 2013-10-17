83df27221c3f4324b4cb5f2d92def36aa16231e4
Adrian Philipp <mail@adrian-philipp.com>
2013-10-17 22:37:25 +0200

    Fixed: Moves guzzle namespace from Adri to VCR.

---
+++ guzzle/test/VCR/GithubProjectGuzzleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace VCR;
+
+use Guzzle\Http\Client;
+use Guzzle\Http\Exception\ClientErrorResponseException;
+/**
+* Tests Guzzle class.
+*/
+class GithubProjectGuzzleTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @vcr github_adri_php-vcr.yml
+     */
+    public function testGithubInfoForExistingProject()
+    {
+        $githubProject = new GithubProjectGuzzle('php-vcr/php-vcr');
+        $info = $githubProject->getInfo();
+
+        $this->assertTrue(is_array($info), 'Response is not an array.');
+        $this->assertArrayHasKey('full_name', $info, "Key 'full_name' not found.");
+        $this->assertEquals('php-vcr/php-vcr', $info['full_name'], "Value for key 'full_name' wrong.");
+        $this->assertArrayHasKey('private', $info, "Key 'private' not found.");
+        $this->assertFalse($info['private'], "Key 'private' is not false.");
+    }
+
+    /**
+     * @vcr github_adri_php-vcr.yml
+     */
+    public function testGithubInfoForNonExistingProject()
+    {
+        $githubProject = new GithubProjectGuzzle('php-vcr/random_stuff');
+        $info = $githubProject->getInfo();
+
+        $this->assertNull($info, 'Response is not null.');
+    }
+}
